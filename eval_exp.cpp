8fc353a72a7554546bc59341105d11feb3b790b9
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-26 10:37:45 +0300

    sol 3

--- eval_exp.cpp
+++ eval_exp.cpp
@@ -1,64 +1,55 @@
 #include <iostream>
 #include <cstring>
-#define LGMAX 52
+#define LGMAX 51
 using namespace std;
-bool cifra(char);
+bool ok;
+int poz, lg;
+long long p=1, rez;
+char semn='+', op[]="-+", e[LGMAX];
+void evalExp();
 int main()
 {
-	char e[LGMAX], stvOp[LGMAX/2];
-	long long stvNr[LGMAX/2], nr, rez;
-	int vfOp, vfNr, i, lg;
-	cin.getline(e, LGMAX);
-	vfOp = vfNr = -1;
-	stvOp[++vfOp] = '+';
-	for (lg = strlen(e), nr = i = 0; i <= lg; i++)
-	{
-		if (!cifra(e[i]))
-		{
-			if (stvOp[vfOp] == '*')
-			{
-				stvNr[vfNr] *= nr;
-				vfOp--;
-			}
-			else
-			{
-				stvNr[++vfNr] = nr;
-			}
-			nr = 0;
-			if (i < lg)
-			{
-				stvOp[++vfOp] = e[i];
-			}
-		}
-		else
-		{
-			nr = nr*10 + (e[i]-'0');
-		}
-	}
-	rez = 0;
-	while (vfNr >= 0 && vfOp >= 0)
-	{
-		if(stvOp[vfOp] == '+')
-		{
-			rez += stvNr[vfNr];
-		}
-		else if (stvOp[vfOp] == '-')
-		{
-			rez -= stvNr[vfNr];
-		}
-		else
-		{
-			cout << "Ceva nu a mers bine.\n";
-			return 1;
-		}
-		vfNr--;
-		vfOp--;
-	}
+	cin.getline(e, LGMAX);	
+	lg = strlen(e);
+	evalExp();
 	cout << rez;
 	return 0;
 }
 bool cifra(char c)
 {
 	return c >= '0' && c <= '9';
+}
+void evalExp()
+{
+	if (poz > lg)
+	{
+		return;
+	}
+	long long nr;
+	for (nr = 0; cifra(e[poz]); poz++)
+	{
+		nr = nr*10 + (e[poz]-'0');
+	}
+	if (e[poz] == '*')
+	{
+		p *= nr;
+		ok = 1;
+	}
+	else
+	{
+		if (ok)
+		{
+			nr *= p;
+			p = 1;
+			ok = 0;
+		}
+		rez = (semn == '+') ? rez+nr : rez-nr;
+		if (strchr(op, e[poz]))
+		{
+			semn = e[poz];
+		}
+	}
+	poz++;
+	evalExp();
 }
 // scor 100
